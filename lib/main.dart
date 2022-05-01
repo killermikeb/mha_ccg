9ab5926566b62adab15a978a35b9e41fbe1afb98
killermikeb <killermikeb@googlemail.com>
2022-05-01 20:32:08 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_cards/main.dart' as fcm;
+
+const TAG = "parent.main";
+const CARD_TYPE = "MHA";
+const CARD_CODE = "MHA";
+
+void main() {
+  fcm.main(cardType: CARD_TYPE, cardCode: CARD_CODE);
+}
